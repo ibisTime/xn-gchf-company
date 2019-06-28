7a0384e2b343db66edfcf9d9136a6ac8dfb9a4f7
John Doe <johndoe@example.com>
2019-06-28 10:45:54 +0800

    添加video

---
+++ src/container/biz/project/videoMenu/video-addedit.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Form } from 'antd';
+import { getQueryString, getUserId, getOrganizationCode } from 'common/js/util';
+import DetailUtil from 'common/js/build-detail-dev';
+
+@Form.create()
+class ProjectVideoAddEdit extends DetailUtil {
+    render() {
+        const fields = [{
+            title: '摄像头名称',
+            field: 'cameraName',
+            required: true
+        }, {
+            title: '摄像机账号',
+            field: 'cameraUsername',
+            required: true
+        }, {
+            title: '摄像机密码',
+            field: 'cameraPassword',
+            required: true
+        }, {
+            title: '摄像机IP地址',
+            field: 'cameraIp',
+            required: true
+        }, {
+            title: '摄像头IP端口号',
+            field: 'cameraIpPort',
+            required: true
+        }, {
+            title: '摄像头频道',
+            field: 'cameraChannel',
+            required: true
+        }, {
+            title: '摄像头码流',
+            field: 'cameraBitStream',
+            type: 'select',
+            key: 'bit_stream_type',
+            required: true
+        }, {
+            title: 'CVR主机账号',
+            field: 'cvrHostUsername',
+            required: true
+        }, {
+            title: 'CVR主机密码',
+            field: 'cvrHostPassword',
+            required: true,
+            onChange: (v) => {
+                this.setState({ isBackPay: v === '1' });
+            },
+            formatter: (v) => {
+                if(v && this.state.index < 2) {
+                    this.setState({ isBackPay: +v === 1, index: this.state.index + 1 });
+                }
+                return v;
+            }
+        },
+            {
+                field: 'userId',
+                value: getUserId(),
+                hidden: true
+            }];
+        return this.buildDetail({
+            fields,
+            code: '631855',
+            view: this.view,
+            editCode: 631850,
+            beforeDetail: (params) => {
+                params.userId = getUserId();
+            }
+        });
+    }
+}
+
+export default ProjectVideoAddEdit;
---
+++ src/container/biz/project/videoMenu/video.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import moment from 'moment';
+import {
+    setTableData,
+    setPagination,
+    setBtnList,
+    setSearchParam,
+    clearSearchParam,
+    doFetching,
+    cancelFetching,
+    setSearchData
+} from '@redux/biz/project/video';
+import { listWrapper } from 'common/js/build-list';
+import { showWarnMsg, monthFormat, getUserId, showSucMsg, showErrMsg } from 'common/js/util';
+import fetch from 'common/js/fetch';
+import { Modal } from 'antd';
+
+const confirm = Modal.confirm;
+
+@listWrapper(
+    state => ({
+        ...state.projectVideo,
+        parentCode: state.menu.subMenuCode
+    }),
+    { setTableData, clearSearchParam, doFetching, setBtnList,
+        cancelFetching, setPagination, setSearchParam, setSearchData }
+)
+class ProjectVideo extends React.Component {
+    render() {
+        const fields = [
+            {
+                title: '项目名称',
+                field: 'projectName'
+            }, {
+                title: '摄像头名称',
+                field: 'cameraName',
+                search: true
+            }, {
+                title: '摄像头账户',
+                field: 'cameraUsername'
+            },
+            {
+                title: '摄像头地址',
+                field: 'cameraIp'
+            }, {
+                title: '摄像头端口',
+                field: 'cameraIpPort'
+            }, {
+                title: 'cvr主机用户名',
+                field: 'cvrHostUsername'
+            }];
+        return this.props.buildList({
+            fields,
+            pageCode: 631855,
+            deleteCode: 631851,
+            searchParams: {
+                userId: getUserId()
+            },
+            singleSelect: false,
+            beforeDelete: (params) => {
+                params.userId = getUserId();
+            },
+            btnEvent: {
+                add: () => this.props.history.push('/project/videoMenu/add'),
+                edit: (keys, items) => {
+                    if (!keys.length) {
+                        showWarnMsg('请选择记录');
+                    } else if (keys.length > 1) {
+                        showWarnMsg('请选择一条记录');
+                    } else if (items[0].uploadStatus === '3') {
+                        showWarnMsg('已上传不可修改');
+                    } else if (items[0].uploadStatus === '4' || items[0].uploadStatus === '5') {
+                        showWarnMsg('该状态下不可修改');
+                    } else {
+                        this.props.history.push(`/project/videoMenu/change?code=${keys[0]}`);
+                    }
+                },
+                // 批量删除
+                delete: (keys) => {
+                    const _this = this;
+                    if (!keys.length) {
+                        showWarnMsg('请选择记录');
+                    } else {
+                        confirm({
+                            title: '删除',
+                            content: '是否删除？',
+                            onOk() {
+                                fetch('631851', { codeList: keys, userId: getUserId() }).then(() => {
+                                    showSucMsg('操作成功');
+                                    setTimeout(() => {
+                                        _this.props.getPageData();
+                                    }, 1000);
+                                });
+                            },
+                            onCancel() {
+                            },
+                            okText: '确定',
+                            cancelText: '取消'
+                        });
+                    }
+                }
+            }
+        });
+    }
+}
+
+export default ProjectVideo;
